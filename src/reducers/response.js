ded904e28f414ad55f2c87701637643bd4e2cd3a
Nicole Gilbert <nicolegilbert16@gmail.com>
2018-02-05 14:06:49 -0800

    Wrote out actions and reducers for handing the answer form / response. Stareted on the response form component

---
+++ src/reducers/response.js
@@ -0,0 +1,33 @@
+import {
+    ADD_RESPONSE_REQUEST,
+    ADD_RESPONSE_SUCCESS,
+    ADD_RESPONSE_ERROR
+} from '../actions/response';
+
+const initialState = {
+    response: '',
+    loaoding: false,
+    error: null
+}
+
+export const responseReducer = (state = initialState, action) => {
+    if(action.type === ADD_RESPONSE_REQUEST) {
+        return Object.assign({}, state, {
+            loading: true,
+            error: null
+        })
+    }
+    else if(action.type === ADD_RESPONSE_SUCCESS) {
+        return Object.assign({}, state, {
+            response: action.response,
+            view: 'feedback'
+        })
+    }
+    else if(action.type === ADD_RESPONSE_ERROR) {
+        return Object.assign({}, state, {
+            error: action.error,
+            loading: false
+        })
+    }
+    return state;
+}
