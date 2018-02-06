59ae2d7a91aa4f67517f300522899accbd6cfbcf
Nicci Williams <nicci@nicciwill.com>
2018-02-06 16:38:36 -0500

    Added hard-coded correct/incorrect feedback messages

--- src/components/board.js
+++ src/components/board.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Questions from './questions';
 import AnswerForm from './answer-form';
+import Feedback from './feedback';
 
 export default function Board(){
   return(
     <div className="board">
+      <Feedback />
       <Questions />
       <AnswerForm />
     </div>
---
+++ src/components/feedback.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import './feedback.css';
+
+export function Feedback(props){
+    if(props.correctAnswer === undefined){
+      return null;
+    }
+    else {
+      if(props.correctAnswer === 'thank you'){
+        return(
+          <div className="feedback">
+            <h1 className="feedback-correct">I'm the correct feedback</h1>
+          </div>
+        )
+      }
+      else{
+        return(
+          <div className="feedback">
+            <h1 className="feedback-wrong">I'm the wrong feedback</h1>
+          </div>
+        )
+      }
+    }
+}
+
+const mapStateToProps = (state, props) => {
+  console.log(state);
+  return {
+    correctAnswer: state.question.currWord.answer
+  }
+}
+
+export default connect(mapStateToProps)(Feedback);
