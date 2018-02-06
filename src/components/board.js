4c7184b42db2500b650a080a3b480a604e75ddec
Nicole Gilbert <nicolegilbert16@gmail.com>
2018-02-06 11:04:29 -0800

    have answer form rendering on screen and logging output to console

--- src/components/board.js
+++ src/components/board.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Questions from './questions';
+import AnswerForm from './answer-form';
 
 export default function Board(){
   return(
     <div className="board">
       <Questions />
+      <AnswerForm />
     </div>
   )
 }
