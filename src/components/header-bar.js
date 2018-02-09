5c609b71cedc056d571934e718486f5529d999ef
Nicole Gilbert <nicolegilbert16@gmail.com>
2018-02-08 17:18:31 -0800

    added styling to login form

--- src/components/header-bar.js
+++ src/components/header-bar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {clearAuth} from '../actions/auth';
 import {clearAuthToken} from '../local-storage';
+import './header-bar.css'
 
 export class HeaderBar extends React.Component {
     logOut() {
--- src/components/login-form.js
+++ src/components/login-form.js
@@ -3,6 +3,7 @@ import {Field, reduxForm, focus} from 'redux-form';
 import Input from './input';
 import {login} from '../actions/auth';
 import {required, nonEmpty} from '../validators';
+import {Link, Redirect} from 'react-router-dom';
 import './login-form.css';
 
 export class LoginForm extends React.Component {
@@ -20,32 +21,36 @@ export class LoginForm extends React.Component {
             );
         }
         return (
-            <form
-                className="login-form"
-                onSubmit={this.props.handleSubmit(values =>
-                    this.onSubmit(values)
-                )}>
-                {error}
-                <label htmlFor="username">Username | Usuario</label>
-                <Field
-                    component={Input}
-                    type="text"
-                    name="username"
-                    id="username"
-                    validate={[required, nonEmpty]}
-                />
-                <label htmlFor="password">Password | Contraseña</label>
-                <Field
-                    component={Input}
-                    type="password"
-                    name="password"
-                    id="password"
-                    validate={[required, nonEmpty]}
-                />
-                <button disabled={this.props.pristine || this.props.submitting}>
-                    Log in | Iniciar sesión
-                </button>
-            </form>
+            <div className="login-container">
+                <form
+                    className="login-form"
+                    onSubmit={this.props.handleSubmit(values =>
+                        this.onSubmit(values)
+                    )}>
+                    {/* {error} */}
+                    {/* <label htmlFor="username">Username | Usuario</label> */}
+                    <Field
+                        component={Input}
+                        type="text"
+                        name="username"
+                        id="username"
+                        validate={[required, nonEmpty]}
+                        placeholder="placeholder"
+                    />
+                    {/* <label htmlFor="password">Password | Contraseña</label> */}
+                    <Field
+                        component={Input}
+                        type="password"
+                        name="password"
+                        id="password"
+                        validate={[required, nonEmpty]}
+                    />
+                    <button className="login-button" disabled={this.props.pristine || this.props.submitting}>
+                    <i className="fas fa-key"></i> Log in | Iniciar sesión
+                    </button>
+                </form>
+                <Link to="/register">Register | Registro</Link>
+            </div>
         );
     }
 }
